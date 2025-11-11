7a21fd663cf0ae59190efb11fb36e9eacfb05547
Vinutha Chandra <vchandra@catchpoint.com>
2025-11-11 18:56:05 +0530

    vooi

--- src/PageOne.jsx
+++ src/PageOne.jsx
@@ -1,9 +1,28 @@
-
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function PageOne() {
   return (
     <div>
+      <Helmet>
+        <title>About</title>
+        <script type="text/javascript">
+          {`
+            // Initialize the queue
+            window.CPRUMDataLayer = window.CPRUMDataLayer || [];
+            function cpRumTag() {
+              window.CPRUMDataLayer.push(arguments);
+            }
+
+            // Queue some events before the "real library" is loaded
+            cpRumTag('excludeBenchMarks');
+            cpRumTag('usePageHide', true);
+            cpRumTag( 'tracepoint', 'tracepointt', 'BenchmarkGtay');
+            cpRumTag( 'indicator', 'indii', 1225);
+          `}
+        </script>
+      </Helmet>
+
       <h1>Welcome to Page One</h1>
       <p>This is some content on the first page.</p>
     </div>
