2c476994112e22746f93045acd5a5ad7b474963f
Vinutha <vchandra@catchpoint.com>
2026-01-30 13:16:40 +0530

    Push latest changes

--- src/PageTwo.jsx
+++ src/PageTwo.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 function PageTwo() {
   const throwError = () => {
@@ -8,6 +9,35 @@ function PageTwo() {
 
   return (
     <div>
+      <Helmet>
+        <title>About</title>
+        <script type="text/javascript">
+          {`
+            // Initialize the queue
+            window.CPRUMDataLayer = window.CPRUMDataLayer || [];
+            function cpRumTag() {
+              window.CPRUMDataLayer.push(arguments);
+            }
+            <script>
+    window.CPRUMDataLayer = window.CPRUMDataLayer || [];
+
+    function cpRumTag() {
+      window.CPRUMDataLayer.push(arguments);
+    }
+
+    cpRumTag('benchmarkLog', true);
+  </script>
+            // Queue some events before the "real library" is loaded
+            cpRumTag('excludeBenchMarks');
+            cpRumTag('usePageHide', false);
+            cpRumTag('variation', 'SPA');
+            cpRumTag('pageGroup', 'yuiiw');
+            cpRumTag('conversion', 20, 1200);
+            cpRumTag('appError', 10, 'undefined error');
+            
+          `}
+        </script>
+      </Helmet>
       <h1>Welcome to Page Two</h1>
       <p>This page contains a button that throws an error.</p>
       <button onClick={throwError}>Click to Throw Error</button>
